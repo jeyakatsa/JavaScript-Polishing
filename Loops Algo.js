ae348d0cd451123ff1c7615d2f3be60991a70c10
Jeremy Akatsa <51394348+jeyakatsa@users.noreply.github.com>
2020-06-13 13:14:06 -0700

    Add files via upload

---
+++ Loops Algo.js
@@ -0,0 +1,12 @@
+//Print odds 1-20://
+for(var num = 1; num < 20; num+=2){
+    console.log(num);
+}
+
+//Sum and Print 1-5://
+var sum = 0;
+for(var num = 1; num <= 5; num++){
+        console.log(num);
+        sum = num + sum;
+        console.log(sum);
+    }
